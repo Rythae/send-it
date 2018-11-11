4f17f790267e03e457e9204baaad1a37515d10c8
rythae <auxano42@gmail.com>
2018-11-11 14:14:10 +0100

    Server set up

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+const app = express();
+
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:true}));
+
+const port = process.env.PORT || 5090;
+const server = app.listen(port, () => {console.log(` port ${port}`)});
+
+export default server;
