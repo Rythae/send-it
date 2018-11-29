6747911d0eaf488ec36afd3279113ba80d0ee327
rythae <auxano42@gmail.com>
2018-11-29 09:59:00 +0100

    Database test set up

--- server/test/parcelSpec.js
+++ server/test/parcelSpec.js
@@ -6,6 +6,7 @@ const should = _should();
 
 use(chaiHttp);
 
+
 describe('/GET /api/v1/parcels', () => {
     it('should return all parcels', (done) => {
         request(server)
@@ -21,7 +22,7 @@ describe('/GET /api/v1/parcels', () => {
 describe('/GET /api/v1/parcels/:id', () => {
     it('should return a specific parcel', (done) => {
         request(server)
-        .get('/api/v1/parcels/7')
+        .get('/api/v1/parcels/2')
         .end((err,res) => {
             res.should.have.status(200);
             res.body.should.be.a('object');
@@ -31,7 +32,7 @@ describe('/GET /api/v1/parcels/:id', () => {
 
     it('should return a 404 status when parcel not found', (done) => {
         request(server)
-        .get('/api/v1/parcels/4')
+        .get('/api/v1/parcels/73')
         .end((err,res) => {
             res.should.have.status(404);
             res.body.should.be.a('object');
@@ -53,26 +54,12 @@ describe('/GET /api/v1/users/:id/parcels', () => {
 });
 
 describe('/PUT /api/v1/parcels/:id/status', () => {
-    // it('should update the status of a parcel', (done) => {
-    //     const status = {
-    //         status: 'processing'
-    //     }
-    //     request(server)
-    //     .put('api/v1/parcels/2/status')
-    //     .send(status)
-    //     .end((err,res) => {
-    //         res.should.have.status(200);
-    //         res.body.should.be.a('object');
-    //         done();
-    //     });
-    // });
-    
-    it('should return a 404 error, parcel not found', (done) => {
+    it('should return a 404 error, No parcels exist for the id', (done) => {
         const status = {
             status: 'processing'
         };
         request(server)
-        .put('/api/v1/parcels/3/status')
+        .put('/api/v1/parcels/71/status')
         .send(status)
         .end((err,res) => {
             res.should.have.status(404);
@@ -80,29 +67,59 @@ describe('/PUT /api/v1/parcels/:id/status', () => {
             done();
         });
     });
-    it('should return a 400 error, status field required', (done) => {
+
+    it('should cancel a Parcel, Parcel successfully cancelled', (done) => {
         const status = {
+            status: 'cancelled'
         };
         request(server)
-        .put('/api/v1/parcels/7/status')
+        .put('/api/v1/parcels/7/cancel')
         .send(status)
         .end((err,res) => {
-            res.should.have.status(400);
+            res.should.have.status(200);
             res.body.should.be.a('object');
             done();
         });
     });
+
+    it('it should update the status of a Parcel, Status successfully changed', (done) => {
+        const test_status = {
+            status: "processing"
+        }
+        request(server)
+            .put('/api/v1/parcels/3/status')
+            .send(test_status)
+            .end((err, res) => {
+                res.should.have.status(200);
+                res.body.should.be.a('object');
+                done();
+            });
+        });
+
+//    it('should return a 400 error, status field required', (done) => {
+//         const status = {
+//         };
+//         request(server)
+//         .put('/api/v1/parcels/7/status')
+//         .send(status)
+//         .end((err,res) => {
+//             res.should.have.status(400);
+//             res.body.should.be.a('object');
+//             done();
+//         }); 
+//     });
 });
 
 describe('/POST /api/v1/parcels', () => {
     it('should create a parcel', (done) => {
         const parcel = {
-            title: "laptop",
-            price: 4000,
-            destination: "Abuja",
-            pickUpLocation: "Lagos",
-            presentLocation: "Benin",
-            status: "pending"
+            placedBy: 1,
+            weight: "500KG",
+            weightMetric: "200KG",
+            status: "pending",
+            to: "Lagos",
+            from: "Abuja",
+            currentLocation: "Benin"
         };
         request(server)
             .post('/api/v1/parcels')
@@ -113,90 +130,6 @@ describe('/POST /api/v1/parcels', () => {
                 done();
             });
     });
-    it('should return a 400 error, title field required', (done) => {
-        const parcel = {
-            price: 4000,
-            destination: "Abuja",
-            pickUpLocation: "Lagos",
-            presentLocation: "Benin",
-            status: "pending"
-        };
-        request(server)
-        .post('/api/v1/parcels')
-        .send(parcel)
-        .end((err,res) => {
-            res.should.have.status(400);
-            res.body.should.be.a('object');
-            done();
-        });
-    });
-
-    it('should return a 400 error, price field required', (done) => {
-        const parcel = {
-            title: "laptop",
-            destination: "Abuja",
-            pickUpLocation: "Lagos",
-            presentLocation: "Benin",
-            status: "pending"
-        };
-        request(server)
-        .post('/api/v1/parcels')
-        .end((err,res) => {
-            res.should.have.status(400);
-            res.body.should.be.a('object');
-            done();
-        });
-    });
-
-    it('should return a 400 error, destination field required', (done) => {
-        const parcel = {
-            title: "laptop",
-            pickUpLocation: "Lagos",
-            price: 5000,
-            presentLocation: "Benin",
-            status: "pending"
-        };
-        request(server)
-        .post('/api/v1/parcels')
-        .end((err,res) => {
-            res.should.have.status(400);
-            res.body.should.be.a('object');
-            done();
-        });
-    });
-
-    it('should return a 400 error, presentLocation field required', (done) => {
-        const parcel = {
-            title: "laptop",
-            pickUpLocation: "Lagos",
-            price: 5000,
-            destination: "Abuja",
-            status: "pending"
-        };
-        request(server)
-        .post('/api/v1/parcels')
-        .end((err,res) => {
-            res.should.have.status(400);
-            res.body.should.be.a('object');
-            done();
-        });
-    });
-
-    it('should return a 400 error, status field required', (done) => {
-        const parcel = {
-            title: "laptop",
-            pickUpLocation: "Lagos",
-            price: 5000,
-            destination: "Abuja",
-            presentLocation: "Ibadan"
-        };
-        request(server)
-        .post('/api/v1/parcels')
-        .end((err,res) => {
-            res.should.have.status(400);
-            res.body.should.be.a('object');
-            done();
-        });
-    });
+    
 });
 
